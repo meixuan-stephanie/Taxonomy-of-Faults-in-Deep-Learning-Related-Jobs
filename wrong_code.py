deff528098d15273ba90fff2225efcd55dafa69f
meixuan-stephanie <67006512+meixuan-stephanie@users.noreply.github.com>
2020-07-01 16:31:44 +0800

    Update wrong_code.py

--- wrong_code.py
+++ wrong_code.py
@@ -1,9 +1,10 @@
-# Part II Wrong code(Please note thet only partial code is included for the emphasis of topics)
+# Part II Wrong code
+#Please note thet only partial code is included for the emphasis of topics and the specific part needs to be changed and has been changed is marked with a sign and descriptions
 
 # @hyperparameters examples
 
 learning_rate=0.01
-epochs=10
+epochs=10 #need to be fixed
 my_batch_size=12
 
 my_model = build_model(learning_rate)
@@ -26,3 +27,16 @@ plot_the_loss_curve(epochs, rmse)
 
 #Examine the output above,the loss is not coverging to stay steady which indicates that some hyperparameters are suboptimal. Since the traing loss didnot converge by inspection. One possible solution is to train for more epochs.
 #Please refer to the same topic in another file named correct_code.py
+
+#You may want your models to converge more quickly when your epochs number is set large
+#however, setting the learning rate too large often makes it impossible for a model to converge
+
+learning_rate=100 #needs to be fixed
+epochs=500 
+
+my_model = build_model(learning_rate)
+trained_weight, trained_bias, epochs, rmse = train_model(my_model, my_feature, 
+                                                         my_label, epochs,
+                                                         my_batch_size)
+plot_the_model(trained_weight, trained_bias, my_feature, my_label)
+plot_the_loss_curve(epochs, rmse)
