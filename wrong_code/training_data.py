682f40e496c74b59e7aa26228f6a50beaa1c045b
meixuan-stephanie <67006512+meixuan-stephanie@users.noreply.github.com>
2020-07-02 19:53:06 +0800

    Update training_data.py

--- wrong_code/training_data.py
+++ wrong_code/training_data.py
@@ -1,1 +1,11 @@
+# Part III Correct code (Please note that only partial code is included for the emphasis of topics and the specific part needs to be changed and has been changed is marked with a sign and descriptions)
+
+# @Training data examples
+
+#.           longitude	latitude	housing_median_age	total_rooms	total_bedrooms	population	households	median_income	median_house_value
+#0	        -114.3	      34.2	           15.0.     	5612.0	          1283.0	     1015.0	   472.0	       1.5	          66.9
+#1	        -114.5.     	34.4	           19.0	      7650.0	          1901.0	     1129.0	   463.0	       1.8	          80.1
+#2	        -114.6.     	33.7	           17.0	      720.0	             174.0	      333.0	   117.0	       1.7	          85.7
+#3	        -114.6	      33.6	           14.0	      1501.0	           337.0	      515.0	   226.0	       3.2	          73.4
+#4	        -114.6	      33.6	           20.0	      1454.0	           326.0	      624.0	   262.0	       1.9	          65.5
 
