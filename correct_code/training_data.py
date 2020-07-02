05c124dac5d0a3f8822603855c97448f138e3c03
meixuan-stephanie <67006512+meixuan-stephanie@users.noreply.github.com>
2020-07-02 21:09:19 +0800

    Update training_data.py
    done

--- correct_code/training_data.py
+++ correct_code/training_data.py
@@ -18,10 +18,21 @@ training_df.corr()
 #longitude	       1.0	      -0.9	       -0.1	            0.0	         0.1	          0.1	       0.1	        -0.0	        -0.0	             -0.1
 #latitude	      -0.9	       1.0	        0.0	           -0.0        	-0.1	         -0.1	      -0.1	        -0.1	        -0.1	              0.1
 #housing_median_age	-0.1	   0.0	        1.0	           -0.4	        -0.3	         -0.3	      -0.3	        -0.1	         0.1	             -0.1
-#total_rooms.     0.0	      -0.0	       -0.4	            1.0	         0.9	          0.9	       0.9	         0.2	         0.1	              0.1
+#total_rooms     0.0	      -0.0	       -0.4	            1.0	         0.9	          0.9	       0.9	         0.2	         0.1	              0.1
 #total_bedrooms	 0.1	      -0.1	       -0.3	            0.9	         1.0	          0.9	       1.0	        -0.0	         0.0	              0.0
 #population	     0.1	      -0.1	       -0.3	            0.9	         0.9	          1.0	       0.9	        -0.0	        -0.0	             -0.1
 #households	     0.1	      -0.1	       -0.3	            0.9	         1.0	          0.9	       1.0	         0.0	         0.1	             -0.0
-#median_income	  -0.0	      -0.1	       -0.1	            0.2	        -0.0	         -0.0	       0.0	         1.0	         0.7	              0.2
+#median_income	 -0.0	      -0.1	       -0.1	            0.2	        -0.0	         -0.0	       0.0	         1.0	         0.7	              0.2
 #median_house_value	-0.0	  -0.1	        0.1	            0.1	         0.0	         -0.0	       0.1	         0.7	         1.0	              0.2
-#rooms_per_person-0.1	      0.1	         -0.1	            0.1          0.0	         -0.1	      -0.0	         0.2	         0.2                1.0
+#rooms_per_person -0.1	      0.1	         -0.1	            0.1          0.0	         -0.1	      -0.0	         0.2	         0.2                1.0
+
+#As the result shown above, the 'median_income' correlates 0.7 with the label(median_house_value), so median_income might be a good feature
+
+
+#There are many tricks to be applied when choosing a good feature
+#Some tips:
+#Avoid rarely used discrete feature values for example,a feature called "unique_house_id" which means that feature is very specific to every house.Thus, it's a bad feature
+#Prefer clear and obvious meanings for example "house_age: 27"  rather than stating "house_age: 851472000". In some cases, noisy data causes unclear values
+
+
+#Special thanks to google education for inspiring me on knowledge in this chapter
