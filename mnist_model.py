851d21a65b9feaf9cedb19eca635fa23887ef2c8
meixuan-stephanie <stephanie98747789@gmail.com>
2020-07-10 16:57:05 +0800

    adding a trained model with sth wrong

--- mnist_model.py
+++ mnist_model.py
@@ -32,7 +32,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense,Conv2D,Dropout,Flatten,MaxPooling2D
 from tensorflow.keras.optimizers import SGD
 model=Sequential()
-model.add(Conv2D(32,(3,3),activation='relu',input_shape=(28,28,1)))
+model.add(Conv2D(32,(3,3),activation='relu',input_shape=(28,28)))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Conv2D(64,(3,3),activation='relu'))
 model.add(Conv2D(64,(3,3),activation='relu'))
